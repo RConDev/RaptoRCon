28d6e98af735592ba7a13a3cebb0ee11bf0d46b2
maik.schoeneich <macgyverx83@gmail.com>
2013-10-28 22:57:48 +0100

    Beginning of independent Game Infrastructure
    With this type of infrastructure in mind, there will be no issues regarding the support of several games having an administration interface. You don't care about the details of the game. It's an abstraction layer on top of the game. The game will only manage it's stuff and administration starts a higher level. It will not be important how the game manages it, but that it can manage it.
    
    Issue #8

---
+++ src/RaptoRCon.Games/IGame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RaptoRCon.Games
+{
+    /// <summary>
+    /// This interface describes the basic element. A Game which can be administered
+    /// with RaptoRCon
+    /// </summary>
+    public interface IGame
+    {
+        /// <summary>
+        /// Gets the unique identifier of the game within RaptoRCon
+        /// </summary>
+        Guid Id { get; }
+
+        /// <summary>
+        /// Gets the public / official name of the game
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Gets the official <see cref="Uri"/> to the homepage of the game
+        /// </summary>
+        Uri Homepage { get; }
+
+        /// <summary>
+        /// Creates a <see cref="IGameConnection"/> to the specified endpoint
+        /// </summary>
+        /// <param name="hostname"></param>
+        /// <param name="port"></param>
+        /// <returns></returns>
+        Task<IGameConnection> CreateConnectionAsync(IGameConnectionInfo connectionInfo);
+    }
+}
---
+++ src/RaptoRCon.Games/IGameConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaptoRCon.Games
+{
+    public interface IGameConnection
+    {
+        Task ConnectAsync();
+        Task DisconnectAsync();
+        Task SendAsync(IGameCommand command);
+
+        event EventHandler<GameDataEventArgs> GameDataSent;
+        event EventHandler<GameDataEventArgs> GameDataReceived;
+
+        event EventHandler<GameConnectionEventArgs> ConnectionCreated;
+        event EventHandler<GameConnectionEventArgs> ConnectionLost;
+        event EventHandler<GameConnectionEventArgs> ConnectionClosed;
+    }
+}
