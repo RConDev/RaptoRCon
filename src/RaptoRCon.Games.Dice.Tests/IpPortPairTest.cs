96539ad91795425cba646187881f607249323b75
maik.schoeneich <macgyverx83@gmail.com>
2013-11-15 00:11:47 +0100

    Added last basic DICE type IpPortPair
    Issue #19

---
+++ src/RaptoRCon.Games.Dice.Tests/IpPortPairTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RaptoRCon.Games.Dice.Tests
+{
+    public class IpPortPairTest
+    {
+        #region Ctor
+
+        [Fact]
+        public void Ctor_IpAddressNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new IpPortPair(null, 10000));
+        }
+
+        [Fact]
+        public void Ctor_IpAddressNull_ThrowsArgumentNullExceptionParamNameIpAddress()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => new IpPortPair(null, 10000));
+            Assert.Equal("ipAddress", exception.ParamName);
+        }
+
+        [Fact]
+        public void Ctor_Port0_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new IpPortPair(IPAddress.Parse("127.0.0.1"), 0));
+        }
+
+        [Fact]
+        public void Ctor_Port0_ThrowsArgumentOutOfRangeExceptionParamNamePort()
+        {
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => new IpPortPair(IPAddress.Parse("127.0.0.1"), 0));
+            Assert.Equal("port", exception.ParamName);
+        }
+
+        #endregion
+
+        #region ToWords()
+
+        [Fact]
+        public void ToWords_Ip127001Port1024_WordsCount2()
+        {
+            var ipPort = new IpPortPair(IPAddress.Parse("127.0.0.1"), 1024);
+            Assert.Equal(1, ipPort.ToWords().Count());
+        }
+
+        [Fact]
+        public void ToWords_Ip127001Port1024_WordsDiceWord127001Port1024()
+        {
+            var expectedSequence = new[] { new DiceWord("127.0.0.1:1024") };
+            var ipPort = new IpPortPair(IPAddress.Parse("127.0.0.1"), 1024);
+            Assert.Equal(expectedSequence, ipPort.ToWords());
+        }
+
+        #endregion
+    }
+}
---
+++ src/RaptoRCon.Games.Dice/IpPortPair.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace RaptoRCon.Games.Dice
+{
+    /// <summary>
+    /// A string on the following format: <IPv4 address>:<port number>
+    /// </summary>
+    public class IpPortPair : IDiceWordifyable
+    {
+        private readonly IPAddress ipAddress;
+        private readonly ushort port;
+
+        public IpPortPair(IPAddress ipAddress, UInt16 port)
+        {
+            #region Contracts
+            if (ipAddress == null)
+            {
+                throw new ArgumentNullException("ipAddress");
+            }
+
+            if (port == 0)
+            {
+                throw new ArgumentOutOfRangeException("port");
+            }
+            #endregion
+
+            this.ipAddress = ipAddress;
+            this.port = port;
+        }
+
+        public IEnumerable<IDiceWord> ToWords()
+        {
+            var wordContent = string.Format("{0}:{1}", ipAddress, port);
+            yield return new DiceWord(wordContent);
+        }
+    }
+}
